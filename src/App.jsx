c2829b126e7c1c630c557bbf8feadebbb69b80c0
DCL <raihan15-3098@diu.edu.bd>
2023-10-02 14:52:35 +0600

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import{BrowserRoute,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Singin from "./pages/Singin"
+import Singup from "./pages/Singup"
+import About from "./pages/About"
+import Profile from "./pages/profile"
+
+
+
 
 export default function App() {
   return (
-    <div className='text-red-500'> app</div>
+    <BrowserRoute>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Singin" element={<Singin/>}/>
+      <Route path="/Singup" element={<Singup/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Profile" element={<Profile/>}/>
+      
+        
+    </Routes>
+    
+    </BrowserRoute>
   )
 }
